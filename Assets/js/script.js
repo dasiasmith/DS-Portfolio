b56b6c3ca44f4f814662a082cc6bf79f5500f10e
Dasia <dasiasmith023@gmail.com>
2022-03-25 21:14:35 -0500

    Updated the homegape. Added cards

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,37 +1,42 @@
-var demoButtons;
+console.clear();
 
-function start () {
-  
-  // Add event "click" to "demo buttons"
-  demoButtons = document.querySelectorAll ('.js-modify');
-  for (var i = 0; i < demoButtons.length; i++) {
-    demoButtons[i].addEventListener ('click', toggleEffect);
-  }
-  
-  // Add event "click" to "save buttons"
-  var saveButtons = document.querySelectorAll ('.js-save');
-  for (var i = 0; i < saveButtons.length; i++) {
-    saveButtons[i].addEventListener ('click', toggleActive);
-  }
-  
-}
+Splitting();
 
-// Toggle "effect" classes
-function toggleEffect () {
-  var target = document.querySelector (this.dataset.target);
-      target.dataset.effect = this.dataset.effect;
-  
-  for (var i= 0; i < demoButtons.length; i++) {
-    demoButtons[i].classList.remove ('active');
-  }
-  
-  toggleActive.call (this);
-}
 
-// Toggle "active" class
-function toggleActive () {
-  this.classList.toggle ('active');
-}
+// var demoButtons;
 
-// Invoke "start ()" function
-window.addEventListener ('load', start);
+// function start () {
+  
+//   // Add event "click" to "demo buttons"
+//   demoButtons = document.querySelectorAll ('.js-modify');
+//   for (var i = 0; i < demoButtons.length; i++) {
+//     demoButtons[i].addEventListener ('click', toggleEffect);
+//   }
+  
+//   // Add event "click" to "save buttons"
+//   var saveButtons = document.querySelectorAll ('.js-save');
+//   for (var i = 0; i < saveButtons.length; i++) {
+//     saveButtons[i].addEventListener ('click', toggleActive);
+//   }
+  
+// }
+
+// // Toggle "effect" classes
+// function toggleEffect () {
+//   var target = document.querySelector (this.dataset.target);
+//       target.dataset.effect = this.dataset.effect;
+  
+//   for (var i= 0; i < demoButtons.length; i++) {
+//     demoButtons[i].classList.remove ('active');
+//   }
+  
+//   toggleActive.call (this);
+// }
+
+// // Toggle "active" class
+// function toggleActive () {
+//   this.classList.toggle ('active');
+// }
+
+// // Invoke "start ()" function
+// window.addEventListener ('load', start);
